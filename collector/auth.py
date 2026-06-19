32714751408a6ac9362a529acd1ae34742b8fdad
lucas93barros <162639387+lucas93barros@users.noreply.github.com>
2026-06-19 10:43:48 -0300

    Ensure Chromium is installed before login
    Add subprocess call to ensure Chromium installation.

--- collector/auth.py
+++ collector/auth.py
@@ -1,9 +1,17 @@
+
+import subprocess
 from playwright.sync_api import sync_playwright
 from config import LOGIN_URL
 
 
 class SefazAuth:
     def login(self, cpf: str, senha: str):
+
+         # garante instalação do Chromium no Streamlit Cloud
+        subprocess.run(
+            ["playwright", "install", "chromium"],
+            check=True
+        )
         playwright = sync_playwright().start()
 
         browser = playwright.chromium.launch(
@@ -14,10 +22,13 @@ class SefazAuth:
         page = context.new_page()
 
         page.goto(LOGIN_URL)
+
         page.fill('input[name="cod_cpf"]', cpf)
         page.fill('#dsc_senha', senha)
+
         page.click('input[type="submit"]')
         page.wait_for_load_state("networkidle")
+
         if page.locator("text=Lista de Funções").count() == 0:
             raise Exception("Falha na autenticação")
 
